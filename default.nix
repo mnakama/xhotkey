7caf06c59ebe4791a8a91e299a4e8e82467deef0
Matt Nakama <matt+git@mattnakama.com>
2023-01-31 04:18:58 -0500

    default.nix: complete build

--- default.nix
+++ default.nix
@@ -1,5 +1,25 @@
 with import <nixpkgs> {};
-stdenv.mkDerivation {
+stdenv.mkDerivation rec {
   name = "xhotkey";
   buildInputs = [ xlibsWrapper ];
+
+  #src = fetchgit {
+  #  url = "https://github.com/mnakama/${name}";
+  #  rev = "56d47de794ef0c818f582cf407ff2a3c7ae9b930";
+  #  sha256 = "193rifhpgfyma2b49dfx94gmkgvsv0qmfsm2s43dp1fn7kbpr07r";
+  #};
+
+  src = ".";
+
+  unpackPhase = ''
+    cp ${./Makefile} Makefile
+    cp ${./xhotkey.c} xhotkey.c
+  '';
+
+  dontConfigure = true;
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp ./xhotkey $out/bin/xhotkey
+  '';
 }
